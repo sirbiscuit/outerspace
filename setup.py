3be28ead420651a5b9a04c34b845d71c5782692e
Sir Biscuit <sirbiscuit@users.noreply.github.com>
2019-10-18 11:43:22 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='outerspace',
-    version='0.2.1',
+    version='0.2.2',
     description='',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
